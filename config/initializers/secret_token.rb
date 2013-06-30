1094dfbf526c922531634141a9a8cdba960407f5
Abhishek Ray <abhishekray07@gmail.com>
2013-07-01 00:12:47 +0800

    SearchSG setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Searchsg::Application.config.secret_token = 'd48662c878d8baac5be74f2d5b609927ed7f1ea2edd7c71e38aacfcf68b830711a371719b59778a24c5d3a15a4cabc69d77ec976d778bab5c227666002b8d1f7'
