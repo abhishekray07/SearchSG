422372c9df6fe0a8372f1a12709719523a0361ca
Abhishek Ray <abhishekray07@gmail.com>
2013-07-15 00:27:40 +0800

    Open information

--- app/scripts/google.rb
+++ app/scripts/google.rb
@@ -2,8 +2,8 @@ require "net/https"
 require "json"
 require "uri"
 
-uri = URI.parse("https://maps.googleapis.com/maps/api/place/search/json?location=1.31675,103.8904&radius=5000&name=%22restaurant%22&sensor=false&key=AIzaSyCoVITdjn-uJNxBr3qVGyQ69qbXAbbMaXQ")
-# uri = URI.parse("https://maps.googleapis.com/maps/api/place/details/json?reference=CoQBeAAAANOfGqZ_1ijEG9XQuDBYAf3syuSBr9zDlxyupsvJjNA8w6eSArZlt-d-bRw16r3ElboDaT0NgI-88-JPnmUrxzZZe7ITXiq_gncZQWv9HDCagmZPZvd0sT-U3tM1OcrNv5-UqmYZadsiIsmxg2-FT4ioybXPgrGwDn-nwY87ZhUTEhCvA8RksYTnEzouoJIFT43QGhTRocHvkbLBXdrS5C-0UAuOYObZgA&sensor=false&key=AIzaSyCoVITdjn-uJNxBr3qVGyQ69qbXAbbMaXQ")
+uri = URI.parse("https://maps.googleapis.com/maps/api/place/search/json?location=1.31675,103.8904&radius=5000&name=%22fitness+first%22&sensor=false&key=AIzaSyCoVITdjn-uJNxBr3qVGyQ69qbXAbbMaXQ")
+# uri = URI.parse("https://maps.googleapis.com/maps/api/place/details/json?reference=CnRrAAAAu-ws8DX_v7kAqYCXgefeIwgu1KPPnKbFFw9RVv07Z3DIGXs9ZbqnreX4o2wvOC7S7ByKn75gxHCEbOhxs1RWbN5HPhAVCIJ3h3skayYjRVDh1KGzcJfCCMPYK1ciOuQqCKt3mVv4QOhdgWi6QaNnMRIQQCjAOoeJG2RgPlo1vtrWyhoUOt8k28_qrLFYk9Xpmu_NRV-06k4&sensor=false&key=AIzaSyCoVITdjn-uJNxBr3qVGyQ69qbXAbbMaXQ")
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -12,8 +12,11 @@ request = Net::HTTP::Get.new(uri.request_uri)
 
 response = http.request(request)
 hash = JSON.parse response.body
-p hash
+# p hash
 
-# hash["results"].each do |result|
-	# puts "#{result["name"]} => #{result["vicinity"]}" 
-# end
+hash["results"].each do |result|
+	puts "#{result["name"]} => #{result["vicinity"]}" 
+	if result["opening_hours"]
+		puts result["opening_hours"]["open_now"]
+	end
+end
