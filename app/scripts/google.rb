68b7b0658afb1fb3f7cbf440704f875c8ea00d91
Abhishek Ray <abhishekray07@gmail.com>
2013-07-19 22:53:53 +0800

    Added distance locations

--- app/scripts/google.rb
+++ app/scripts/google.rb
@@ -1,6 +1,7 @@
 require "net/https"
 require "json"
 require "uri"
+require "Geocoder"
 
 uri = URI.parse("https://maps.googleapis.com/maps/api/place/search/json?location=1.28967,103.85&radius=5000&name=%22fitness+first%22&sensor=false&key=AIzaSyCoVITdjn-uJNxBr3qVGyQ69qbXAbbMaXQ")
 # uri = URI.parse("https://maps.googleapis.com/maps/api/place/details/json?reference=CnRrAAAAu-ws8DX_v7kAqYCXgefeIwgu1KPPnKbFFw9RVv07Z3DIGXs9ZbqnreX4o2wvOC7S7ByKn75gxHCEbOhxs1RWbN5HPhAVCIJ3h3skayYjRVDh1KGzcJfCCMPYK1ciOuQqCKt3mVv4QOhdgWi6QaNnMRIQQCjAOoeJG2RgPlo1vtrWyhoUOt8k28_qrLFYk9Xpmu_NRV-06k4&sensor=false&key=AIzaSyCoVITdjn-uJNxBr3qVGyQ69qbXAbbMaXQ")
@@ -13,18 +14,23 @@ request = Net::HTTP::Get.new(uri.request_uri)
 response = http.request(request)
 hash = JSON.parse response.body
 
-p hash
+p Geocoder::Calculations.distance_between([1.28967,103.85], [40.748433,-73.985655])
+
+# p hash["results"]
 # p hash["result"]["formatted_address"]
 # p hash["result"]["international_phone_number"]
 # p hash["result"]["name"]
 
 # p hash["result"]["reviews"][0]
-# # hash["results"].each do |result|
-# # 	puts "#{result["name"]} => #{result["vicinity"]} => #{result["reference"]} " 
-# # 	if result["opening_hours"]
-# # 		puts result["opening_hours"]["open_now"]
-# # 	end
-# # end
+hash["results"].each do |result|
+	puts "#{result["name"]} => #{result["vicinity"]} => #{result["reference"]} " 
+	puts "Distance"
+	p Geocoder::Calculations.distance_between([1.28967,103.85], [result["geometry"]["location"]["lat"],result["geometry"]["location"]["lng"]])
+
+	if result["opening_hours"]
+		puts result["opening_hours"]["open_now"]
+	end
+end
 
 # hash["result"]["reviews"].each do |result|
 # 	p result["author_name"]
