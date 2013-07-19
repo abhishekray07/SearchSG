608447e7ec90c4c4ee3d826e93f99560f8f298e7
Abhishek Ray <abhishekray07@gmail.com>
2013-07-19 23:09:43 +0800

    Changed script file

--- app/scripts/google.rb
+++ app/scripts/google.rb
@@ -1,7 +1,6 @@
 require "net/https"
 require "json"
 require "uri"
-require "Geocoder"
 
 uri = URI.parse("https://maps.googleapis.com/maps/api/place/search/json?location=1.28967,103.85&radius=5000&name=%22fitness+first%22&sensor=false&key=AIzaSyCoVITdjn-uJNxBr3qVGyQ69qbXAbbMaXQ")
 # uri = URI.parse("https://maps.googleapis.com/maps/api/place/details/json?reference=CnRrAAAAu-ws8DX_v7kAqYCXgefeIwgu1KPPnKbFFw9RVv07Z3DIGXs9ZbqnreX4o2wvOC7S7ByKn75gxHCEbOhxs1RWbN5HPhAVCIJ3h3skayYjRVDh1KGzcJfCCMPYK1ciOuQqCKt3mVv4QOhdgWi6QaNnMRIQQCjAOoeJG2RgPlo1vtrWyhoUOt8k28_qrLFYk9Xpmu_NRV-06k4&sensor=false&key=AIzaSyCoVITdjn-uJNxBr3qVGyQ69qbXAbbMaXQ")
@@ -25,7 +24,7 @@ p Geocoder::Calculations.distance_between([1.28967,103.85], [40.748433,-73.98565
 hash["results"].each do |result|
 	puts "#{result["name"]} => #{result["vicinity"]} => #{result["reference"]} " 
 	puts "Distance"
-	p Geocoder::Calculations.distance_between([1.28967,103.85], [result["geometry"]["location"]["lat"],result["geometry"]["location"]["lng"]])
+	# p Geocoder::Calculations.distance_between([1.28967,103.85], [result["geometry"]["location"]["lat"],result["geometry"]["location"]["lng"]])
 
 	if result["opening_hours"]
 		puts result["opening_hours"]["open_now"]
